0e494e34d37c21e00c67d5adc2e7ad79d7162ecd
Anthony Guevara <amboxer21@yahoo.com>
2014-11-02 13:17:47 -0500

    Update ImageCapture.sh

--- ImageCapture.sh
+++ ImageCapture.sh
@@ -4,10 +4,9 @@ while true; do
 
         if [[ `ps aux | grep "ruby ImageCapture.rb" | awk 'END {print NR}'` -ne 2 ]]; then
 
-                # No need for the absolute path for anyone else but I'd like to have it.
-                #ruby /home/anthony/Documents/Ruby/ImageCapture.rb;
+                ruby /home/anthony/Documents/Ruby/ImageCapture.rb;
 
-                find / -name "ImageCapture.rb" -exec ruby {} \;
+                #find / -name "ImageCapture.rb" -exec ruby {} \;
 
         fi
 
